cb46fd6fadcd9dd9b7ab828a43eee06b0669e7bc
reykenj <230343s@mymail.nyp.edu.sg>
2024-11-28 03:46:08 +0800

    Rico Boom? Yes Rico Boom

--- 3DGE ICA1/Assets/Scripts/Projectile.cs
+++ 3DGE ICA1/Assets/Scripts/Projectile.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float damage;
+    [SerializeField] private float ExplosionRadius = 1.0f;
+    [SerializeField] private float ExplosionForce = 1.0f;
     public GameObject CollisionEffectPrefab;
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject hitObject = collision.gameObject;
+        //GameObject hitObject = collision.gameObject;
         // Instantiate the impact effect
         GameObject Effect = Instantiate(CollisionEffectPrefab, collision.contacts[0].point,
         Quaternion.LookRotation(collision.contacts[0].normal));
         Destroy(Effect, 3.0f);
-        if (hitObject.TryGetComponent(out Damageable damageable))
+        //if (hitObject.TryGetComponent(out Damageable damageable))
+        //{
+        //    damageable.TakeDamage(damage);
+        //}
+        Collider[] colliders;
+
+        colliders = Physics.OverlapSphere(transform.position, radius: ExplosionRadius);
+
+        for (int i = 0; i < colliders.Length; i++)
         {
-            damageable.TakeDamage(damage);
+            var collider = colliders[i];
+            if (collider != null)
+            {
+                Rigidbody objecthit = collider.GetComponent<Rigidbody>();
+                if (objecthit != null)
+                {
+                    Vector3 explosiondirection = (transform.position - objecthit.transform.position).normalized;
+                    objecthit.AddForce(-explosiondirection * ExplosionForce, ForceMode.Impulse);
+                }
+                if (collider.gameObject.TryGetComponent(out Damageable damageable))
+                {
+                    damageable.TakeDamage(damage);
+                }
+            }
         }
         Destroy(this.gameObject);
     }
